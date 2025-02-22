cab9943a070cec25ef4dbf245621e1b763016d48
Gyasuddin <gyasuddinansari7854@gmail.com>
2025-02-23 00:33:02 +0530

    Home file updated

---
+++ Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+namespace camRental.Controllers
+{
+    public class UserController : Controller
+    {
+        [Authorize(Roles = "User")]  // ❌ Unauthorized Users Yaha Nahi Aa Sakte
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
