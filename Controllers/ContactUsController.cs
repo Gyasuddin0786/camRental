1fc56d746a6f52200cb86e5b047306501027e242
Amankrgupta8252 <agupta183@rku.ac.in>
2025-03-18 11:09:33 +0530

    Hello

---
+++ Controllers/ContactUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace camRental.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
