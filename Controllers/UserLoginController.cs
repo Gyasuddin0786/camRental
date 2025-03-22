eaf83d97072a124f7646a73d3f7555d42bc586c8
Amankrgupta8252 <agupta183@rku.ac.in>
2025-03-22 23:00:34 +0530

    LoginSignUp page

---
+++ Controllers/UserLoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace camRental.Controllers
+{
+    public class UserLoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
